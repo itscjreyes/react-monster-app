4b2bce200b9a75774fa3549939bee91fb19f47d5
CJ Reyes <itscjreyes@gmail.com>
2020-02-12 14:19:24 -0500

    Updates

---
+++ precache-manifest.6eace98723c214b52a2467ad26dfa841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52a57d0b942c070a1e3305a55a277c9",
+    "url": "/react-monster-app/index.html"
+  },
+  {
+    "revision": "46e8563f4b12aa06554e",
+    "url": "/react-monster-app/static/css/main.9f32812c.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/react-monster-app/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-monster-app/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e8563f4b12aa06554e",
+    "url": "/react-monster-app/static/js/main.0d8f1a3f.chunk.js"
+  },
+  {
+    "revision": "0be3a7da61f252252c16",
+    "url": "/react-monster-app/static/js/runtime-main.9621a6e6.js"
+  }
+]);
